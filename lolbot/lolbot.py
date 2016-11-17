f92b85d7754eabebd7330960ef0776567e1b734e
James Berck III <jwberck1@gmail.com>
2016-11-17 00:21:34 -0600

    updated bot to match intended functionality

--- lolbot/lolbot.py
+++ lolbot/lolbot.py
@@ -6,16 +6,33 @@ import os
 from config_lolbot import *
 
 
+if not os.path.isfile("config_lolbot.py"):
+    print("No valid config file found")
+    exit(1)
+
 USER_AGENT = ("LaughingBot 0.1")
 
 MESSAGE = "lol"
 
 r = praw.Reddit(USER_AGENT)
 
-subreddit = r.get_subreddit("me_irl")
+r.login(REDDIT_USERNAME, REDDIT_PASS)
 
-for submission in subreddit.get_hot(limit = 5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score ", submission.score)
-    print("-----------------------------\n")
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+else:
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = filter(None, posts_replied_to)
+
+subreddit = r.get_subreddit("pythonforengineers")
+
+for comment in r.get_comments("pythonforengineers"):
+    body = comment.body.lower()
+    if re.search("lol", body, re.IGNORECASE) and comment.score < -1 and comment.id not in posts_replied_to:
+        comment.reply(MESSAGE)
+        posts_replied_to.add(comment.id)
+        print("Comment Made!")
+
+
