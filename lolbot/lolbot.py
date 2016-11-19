b6cb67e6a2dbec4ed888cbee84144e6c1241bf0f
James Berck III <jwberck1@gmail.com>
2016-11-19 17:00:27 -0600

    further code refactoring

--- lolbot/lolbot.py
+++ lolbot/lolbot.py
@@ -7,21 +7,9 @@ from settings import USER_AGENT, scopes
 from prawoauth2 import PrawOAuth2Mini
 
 
-if not os.path.isfile("config_lolbot.py"):
-    print("No valid config file found")
-    exit(1)
-
-MESSAGE = "lol"
-
-r = praw.Reddit(USER_AGENT)
-
-oauth_helper = PrawOAuth2Mini(r, app_key=app_key,
-                              app_secret=app_secret, access_token=access_token,
-                              scopes=scopes, refresh_token=refresh_token)
-
-#defines main functionality of lolbot
+# defines main functionality of lolbot
 def lolbot_loop(comments, posts_replied_to):
-
+    MESSAGE = "lol"
     for comment in comments:
         body = comment.body.lower()
         if re.search("python", body, re.IGNORECASE) and comment.score < 5 and comment.id not in posts_replied_to:
@@ -32,21 +20,29 @@ def lolbot_loop(comments, posts_replied_to):
             print("Comment added to posts_replied_to.txt!")
             exit(1)
 
-#gets the posts_replied_to file
+
+if not os.path.isfile("config_lolbot.py"):
+    print("No valid config file found")
+    exit(1)
+
+reddit_instance = praw.Reddit(USER_AGENT)
+oauth_helper = PrawOAuth2Mini(reddit_instance, app_key=app_key,
+                              app_secret=app_secret, access_token=access_token,
+                              scopes=scopes, refresh_token=refresh_token)
+
+# gets the posts_replied_to file
 if not os.path.isfile("posts_replied_to.txt"):
-     posts_replied_to = []
+     reddit_posts_replied_to = []
 else:
     with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = filter(None, posts_replied_to)
+        reddit_posts_replied_to = f.read()
+        reddit_posts_replied_to = reddit_posts_replied_to.split("\n")
+        reddit_posts_replied_to = filter(None, reddit_posts_replied_to)
 
-
-
-#handles Oauth refreshing
+# handles Oauth refreshing
 while True:
     try:
-        comments = r.get_comments("pythonforengineers")
-        lolbot_loop(comments, posts_replied_to)
+        reddit_comments = reddit_instance.get_comments("pythonforengineers")
+        lolbot_loop(reddit_comments, reddit_posts_replied_to)
     except praw.errors.OAuthInvalidToken:
         oauth_helper.refresh()
