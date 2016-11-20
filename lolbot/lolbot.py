4e66f12d08714bfbc85b1ff596d1225fd325668b
James Berck III <jwberck1@gmail.com>
2016-11-19 19:59:58 -0600

    Can now search through 1000 submissions and all comments within them

--- lolbot/lolbot.py
+++ lolbot/lolbot.py
@@ -8,38 +8,50 @@ from prawoauth2 import PrawOAuth2Mini
 
 
 # defines main functionality of lolbot
-def lolbot_loop(comments, posts_replied_to):
-    MESSAGE = "lol"
-    for comment in comments:
+def lolbot_loop(submissions, posts_replied_to):
+    message = "lol"
 
-        author = comment.author.name
+    submission_count = 0
 
-        # prevents bot from responding to its self
-        if author == "LaughingBot":
-            print("found author")
-            continue
+    for submission in submissions:
+        flat_comments = praw.helpers.flatten_tree(submission.comments)
+        comment_count = 0
+        for comment in flat_comments:
+            try:
+                if comment.score < -5:
+                    print(comment.author)
+                    print(comment.score)
+                    print(comment.body)
+            except Exception:
+                print("found more comment object")
+                continue
 
-        if comment.score < -5:
-            print(comment.author)
-            print(comment.score)
-            print(comment.body)
+            # searches a subreddit for the phrase "lol". if it is downvoted enough, it will respond with "lol"
+            body = comment.body.lower()
+            if re.search("lol", body, re.IGNORECASE) and comment.score < -10 and comment.id not in posts_replied_to:
 
+                # prevents bot from responding to its self
+                author = comment.author.name
+                if author == "LaughingBot":
+                    print("found author")
+                    continue
 
-        # searches a subreddit for the phrase "lol". if it is downvoted enough, it will respond with "lol"
-        body = comment.body.lower()
-        if re.search("lol", body, re.IGNORECASE) and comment.score < -10 and comment.id not in posts_replied_to:
-            print("made it into if")
-            comment.reply(MESSAGE)
-            print("made it past reply")
-            posts_replied_to.append(comment.id)
-            print("Comment added to posts_replied_to")
-            # Write our updated list back to the file
-            with open("posts_replied_to.txt", "w") as f:
-                for post_id in reddit_posts_replied_to:
-                    f.write(post_id + "\n")
-            print("updated posts_replied_to written to file")
-            exit(1)
-
+                print("made it into if")
+                comment.reply(message)
+                print("made it past reply")
+                posts_replied_to.append(comment.id)
+                print("Comment added to posts_replied_to")
+                # Write our updated list back to the file
+                with open("posts_replied_to.txt", "w") as f:
+                    for post_id in reddit_posts_replied_to:
+                        f.write(post_id + "\n")
+                print("updated posts_replied_to written to file")
+                exit(1)
+            comment_count += 1
+            print("Comment Count: {}".format(comment_count))
+        submission_count += 1
+        print("Submission count: {}".format(submission_count))
+    exit(1)
 
 
 if not os.path.isfile("config_lolbot.py"):
@@ -55,8 +67,8 @@ oauth_helper = PrawOAuth2Mini(reddit_instance, app_key=app_key,
 if not os.path.isfile("posts_replied_to.txt"):
      reddit_posts_replied_to = []
 else:
-    with open("posts_replied_to.txt", "r") as f:
-        reddit_posts_replied_to = f.read()
+    with open("posts_replied_to.txt", "r") as p:
+        reddit_posts_replied_to = p.read()
         reddit_posts_replied_to = reddit_posts_replied_to.split("\n")
         reddit_posts_replied_to = filter(None, reddit_posts_replied_to)
 
@@ -65,8 +77,10 @@ while True:
     try:
         print("in high level loop")
         subreddit = reddit_instance.get_subreddit("me_irl")
-        reddit_comments = subreddit.get_comments(limit=None)
-        lolbot_loop(reddit_comments, reddit_posts_replied_to)
+        print("got reddit instance")
+        reddit_submissions = subreddit.get_hot(limit=1024)
+        print("got reddit submissions")
+        lolbot_loop(reddit_submissions, reddit_posts_replied_to)
     except praw.errors.OAuthInvalidToken:
         oauth_helper.refresh()
         print("oauth refreshed")
